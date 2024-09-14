323ce6d3bfcc58c366181016f2c9e36666cbb4c4
Bobur Yusupov <bobur.yu06@mail.ru>
2024-09-14 15:21:43 +0200

    Added comment features

--- blog-application/blog/views.py
+++ blog-application/blog/views.py
@@ -2,10 +2,11 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 class PostListView(ListView):
     queryset = Post.objects.all()
@@ -44,12 +45,17 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
+
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
     
     return render(
         request,
         'blog/post/detail.html',
         {
-            "post": post
+            "post": post,
+            "comments": comments,
+            "form": form
         }
     )
 
@@ -97,4 +103,32 @@ def post_share(request, post_id):
             'form': form,
             'sent': sent
         },
+    )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+
+    comment=None
+
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+
+        comment.save()
+
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        }
     )
