d521fb01860d7829a19a1fc7101fd0f78ff05147
Bobur Yusupov <bobur.yu06@mail.ru>
2024-10-01 16:23:52 +0200

    Password change feature added

--- social-website/app/accounts/urls.py
+++ social-website/app/accounts/urls.py
@@ -8,5 +8,9 @@ from .views import dashboard
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path("password-change/", auth_views.PasswordChangeView.as_view(template_name="registration/password_change.html"), name="password-change"),
+    path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(), name="password-change-done"),
+
     path("", dashboard, name="dashboard"),
 ]
